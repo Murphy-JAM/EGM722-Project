83195668d838f8c02caef934074eceac0fab2def
Murphy-JAM <r3.exup@gmail.com>
2023-05-08 16:46:05 +0100

    Imported os and opened data files

--- Belfast_Greenspace_Project_Script.py
+++ Belfast_Greenspace_Project_Script.py
@@ -5,4 +5,11 @@ import notebook
 import rasterio as rio
 import pyepsg
 import folium
+import os
+
+# open data files
+parks = gpd.read_file(os.path.abspath('JAM_data/Greenspace_Python_Project_Data/Belfast_Parks.shp'))
+residential_properties = gpd.read_file(os.path.abspath('JAM_data/Greenspace_Python_Project_Data/Belfast_Pointer.shp'))
+small_areas = gpd.read_file(os.path.abspath('JAM_data/Greenspace_Python_Project_Data/Belfast_Small_Areas.shp'))
+
 
