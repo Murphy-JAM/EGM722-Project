efa8f64e6cd2e11a3667dc1df326add1e11f0632
Murphy-JAM <r3.exup@gmail.com>
2023-05-09 18:02:59 +0100

    Aim of project changed

--- Belfast_Greenspace_Project_Script.py
+++ Belfast_Greenspace_Project_Script.py
@@ -8,6 +8,6 @@ import folium
 import os
 
 # open data files
-parks = gpd.read_file('C:\\Users\\Julie\\Desktop\\Documents\\PostGrad\\Python Assess\\Project\\Greenspace_Python_Project_Data\\Belfast_Parks.shp')
-residential_properties = gpd.read_file('C:\\Users\\Julie\\Desktop\\Documents\\PostGrad\\Python Assess\\Project\\Greenspace_Python_Project_Data\\Belfast_Pointer.shp')
-small_areas = gpd.read_file('C:\\Users\\Julie\\Desktop\\Documents\\PostGrad\\Python Assess\\Project\\Greenspace_Python_Project_Data\\Belfast_Small_Areas.shp')
+
+
+
