d3520d72fa0c226475306f9265cba14a539cf276
Murphy-JAM <r3.exup@gmail.com>
2023-05-09 19:16:19 +0100

    Created geodataframe for Belfast Parks

--- Belfast_Greenspace_Project_Script.py
+++ Belfast_Greenspace_Project_Script.py
@@ -15,6 +15,15 @@ smallareas.head(5)
 # use the small areas size (ha) to visualise the polygons and set colour scheme
 m = smallareas.explore('Hectares', cmap='viridis')
 
+# read the Belfast parks csv file
+df = pd.read_csv('C:\\Users\\Julie\\Desktop\\Documents\\PostGrad\\Python Assess\\Project\\New_Data_Project\\Belfast_Parks.csv')
+
+# create a new geodataframe using the name, address and postcode columns of the csv file.
+# set the geometry using points from xy
+# set the coordinate reference system using WGS84 code
+parks = gpd.GeoDataFrame(df[['NAME','ADDRESS','POSTCODE']], geometry=gpd.points_from_xy(df['LONGITUDE'], df['LATITUDE']), crs='epsg:4326')
+
+
 
 
 
