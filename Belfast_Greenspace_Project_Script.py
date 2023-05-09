3b69e89aff9041e9ed702815a91736f564cd5e57
Murphy-JAM <r3.exup@gmail.com>
2023-05-09 18:58:27 +0100

    Visualise small areas by size and set colour scheme

--- Belfast_Greenspace_Project_Script.py
+++ Belfast_Greenspace_Project_Script.py
@@ -8,8 +8,14 @@ import folium
 import os
 import pandas as pd
 
-# open small area file
+# open small area file and preview data
 smallareas = gpd.read_file('C:\\Users\\Julie\\Desktop\\Documents\\PostGrad\\Python Assess\\Project\\New_Data_Project\\Belfast_SmallAreas.shp')
+smallareas.head(5)
+
+# use the small areas size (ha) to visualise the polygons and set colour scheme
+m = smallareas.explore('Hectares', cmap='viridis')
+
+
 
 
 
